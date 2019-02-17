573591b17ad086b71746b4f0b886ab662ec61f15
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-02-17 17:50:31 +1100

    Создано исключение MyArraySizeException

--- src/Lesson2/MyArray.java
+++ src/Lesson2/MyArray.java
@@ -8,4 +8,35 @@ package Lesson2;
  */
 
 public class MyArray {
+
+    // Задаем размерность массива
+    private final int ARRAY_SIZE_I=4;
+    private final int ARRAY_SIZE_J=4;
+
+    private String[][] array;
+
+    public MyArray(String[][] array) {
+        this.array = array;
+        if (!checkSizeArray()) {
+            throw new MyArraySizeException(ARRAY_SIZE_I, ARRAY_SIZE_J);
+        }
+    }
+
+    // Проверка размерности массива
+    private boolean checkSizeArray() {
+
+        if (array.length != ARRAY_SIZE_I) {
+            return false;
+        }
+        else {
+            for (int i = 0; i < ARRAY_SIZE_I; i++) {
+                if (array[i].length != ARRAY_SIZE_J) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
 }
--- src/Lesson2/TestException.java
+++ src/Lesson2/TestException.java
@@ -1,11 +1,22 @@
 package Lesson2;
 
 /**
- * Java. Level 2. Lesson X. Homework.
+ * Java. Level 2. Lesson 2. Homework.
  *
  * @author Dmitry Kozhinov d.kozhinov@mail.ru
  * Created on 17.02.2019
  */
 
-public class MyException {
+public class TestException {
+
+    public static void main(String[] args) {
+        String[][] array = { {"11","12","13","14"}, {"21","22","23","24","q"}, {"31","32","33","34"}, {"41","42","43","44"} };
+
+        try {
+            MyArray myArray = new MyArray(array);
+        }  catch (MyArraySizeException e) {
+            System.out.println(e.printException());
+        }
+
+    }
 }
