4e8ef0c485fa03dac6b7f627ab3a5f0806342103
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-02-20 22:14:29 +1100

    Сделал первое задание

---
+++ src/Lesson3/Lesson3Launcher.java
@@ -0,0 +1,11 @@
+package Lesson3;
+
+/**
+ * Java. Level 2. Lesson X. Homework.
+ *
+ * @author Dmitry Kozhinov d.kozhinov@mail.ru
+ * Created on 20.02.2019
+ */
+
+public class Lesson3Launcher {
+}
