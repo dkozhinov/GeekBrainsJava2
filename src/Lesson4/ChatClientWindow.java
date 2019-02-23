dab66b5b1486754fd773ce54fbf12cd5dad0d0bd
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-02-24 00:56:55 +1100

    Start

---
+++ src/Lesson4/ChatClientWindow.java
@@ -0,0 +1,11 @@
+package Lesson4;
+
+/**
+ * Java. Level 2. Lesson X. Homework.
+ *
+ * @author Dmitry Kozhinov d.kozhinov@mail.ru
+ * Created on 23.02.2019
+ */
+
+public class ChatClientWindow {
+}
