62805c164fb581a5b867e196b34537220d1b8de2
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-02-27 14:11:26 +1100

    Выполнено задание

---
+++ src/Lesson5/Lesson5Launcher.java
@@ -0,0 +1,11 @@
+package Lesson5;
+
+/**
+ * Java. Level 2. Lesson X. Homework.
+ *
+ * @author Dmitry Kozhinov d.kozhinov@mail.ru
+ * Created on 27.02.2019
+ */
+
+public class Lesson5Launcher {
+}
