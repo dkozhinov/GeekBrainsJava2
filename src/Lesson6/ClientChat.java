afa65991e9dc1dfe9ba4d0efe6087b5237d8ad5a
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-03-03 23:03:17 +1100

    Начало выполнения ДЗ на основе кода преподавателя

--- src/Lesson6/ClientChat.java
+++ src/Lesson6/ClientChat.java
@@ -1,11 +1,54 @@
 package Lesson6;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
- * Java. Level 2. Lesson X. Homework.
+ * Java. Level 2. Lesson 6. Homework.
  *
  * @author Dmitry Kozhinov d.kozhinov@mail.ru
  * Created on 03.03.2019
  */
 
 public class ClientChat {
+    public static void main(String[] args)
+    {
+        try (Socket s = new Socket("localhost", 3345);
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             DataOutputStream dataOutputStream = new DataOutputStream(s.getOutputStream());
+             DataInputStream dataInputStream = new DataInputStream(s.getInputStream()))
+        {
+
+            System.out.println("Client connected with server.");
+
+            while (!s.isOutputShutdown())
+            {
+                if (br.ready())
+                {
+                    String msg = br.readLine();
+
+                    dataOutputStream.writeUTF(msg);
+                    dataOutputStream.flush();
+                    System.out.println("Client sent message=" + msg);
+
+                    if (msg.equalsIgnoreCase("quit")) {
+                        System.out.println("Client kill connection.");
+                        break;
+                    }
+
+                }
+            }
+
+            System.out.println("Connection is closed.");
+        }
+        catch (Exception e)
+        {
+
+        }
+
+    }
+
 }
--- src/Lesson6/ServerChat.java
+++ src/Lesson6/ServerChat.java
@@ -1,5 +1,11 @@
 package Lesson6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Java. Level 2. Lesson 6. Homework.
  *
@@ -7,5 +13,55 @@ package Lesson6;
  * Created on 03.03.2019
  */
 
-public class Server {
+public class ServerChat {
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            System.out.println("Server is started.");
+            //Server starts on port 3345
+            ServerSocket server = new ServerSocket(3345);
+
+            // We waiting connection with client.
+            Socket client = server.accept();
+            System.out.println("Connection was successful statemented.");
+
+            // Stream uses to send message
+            DataOutputStream dataOutputStream = new DataOutputStream(client.getOutputStream());
+            // Stream uses to receive message
+            DataInputStream dataInputStream = new DataInputStream(client.getInputStream());
+
+            while (!client.isClosed())
+            {
+                dataOutputStream.writeUTF("Server is waiting info from client...");
+                dataOutputStream.flush();
+                System.out.println("Server is waiting info from client...");
+
+                String msg = dataInputStream.readUTF();
+                System.out.println("Read info from client=" + msg);
+
+                if (msg.equalsIgnoreCase("quit"))
+                {
+                    System.out.println("Received quit message from client and session will be closed.");
+                    dataOutputStream.writeUTF("Received quit message from client and session will be closed.");
+                    dataOutputStream.flush();
+                    break;
+                }
+
+            }
+
+            System.out.println("Client disconnected");
+            dataInputStream.close();
+            dataOutputStream.close();
+            client.close();
+            System.out.println("Server is stopped.");
+
+        }
+        catch (IOException e)
+        {
+            System.out.println("Something is wrong!");
+            e.printStackTrace();
+        }
+    }
 }
