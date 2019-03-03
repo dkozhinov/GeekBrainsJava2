8898d57bdb297f01a65cc65ec2051825724ac747
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-03-03 23:02:55 +1100

    Начало выполнения ДЗ

---
+++ src/Lesson6/ClientChat.java
@@ -0,0 +1,11 @@
+package Lesson6;
+
+/**
+ * Java. Level 2. Lesson X. Homework.
+ *
+ * @author Dmitry Kozhinov d.kozhinov@mail.ru
+ * Created on 03.03.2019
+ */
+
+public class ClientChat {
+}
---
+++ src/Lesson6/ServerChat.java
@@ -0,0 +1,11 @@
+package Lesson6;
+
+/**
+ * Java. Level 2. Lesson 6. Homework.
+ *
+ * @author Dmitry Kozhinov d.kozhinov@mail.ru
+ * Created on 03.03.2019
+ */
+
+public class Server {
+}
