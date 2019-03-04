f5dd7378c87ea692cbb29048935cda8b3ceb894c
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-03-04 23:59:46 +1100

    Созданы классы

--- src/Lesson6/ClientChat.java
+++ src/Lesson6/ClientChat.java
@@ -1,9 +1,6 @@
 package Lesson6;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -14,48 +11,101 @@ import java.net.Socket;
  */
 
 public class ClientChat {
-    public static void main(String[] args)
-    {
+
+    private final String SERVER_ADDRESS = "localhost" ;
+    private final int SERVER_PORT = 3345 ;
+    private Socket socket;
+    private DataOutputStream dataOutputStream;
+    private DataInputStream dataInputStream;
+    private BufferedReader bufferedReader;
+    private String outputMessage, inputMessage;
 
 
-        try (Socket s = new Socket("localhost", 3345);
-             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             DataOutputStream dataOutputStream = new DataOutputStream(s.getOutputStream());
-             DataInputStream dataInputStream = new DataInputStream(s.getInputStream()))
-        {
 
-            System.out.println("Client connected with server.");
-            String outputMessage = dataInputStream.readUTF();
-            System.out.println("Read info from server [" + outputMessage + "]");
 
-            while (!s.isOutputShutdown())
-            {
-                if (br.ready())
-                {
-                    String inputMessage = br.readLine();
+
+    public void start() {
+
+        // Open Chat connection
+        try {
+            openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Reading from DataInputStream
+        Thread readingClientTread = new ReadingClientThread();
+        readingClientTread.start();
+
+
+        // Writing to the DataOutputStream
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            while (!socket.isOutputShutdown()) {
+                if (bufferedReader.ready()) {
+                    inputMessage = bufferedReader.readLine();
 
                     dataOutputStream.writeUTF(inputMessage);
                     dataOutputStream.flush();
                     System.out.println("Client sent message [" + inputMessage + "]");
-
-                    outputMessage = dataInputStream.readUTF();
-                    System.out.println("Read info from server [" + outputMessage + "]");
+                    Thread.sleep(1000);
 
                     if (inputMessage.equalsIgnoreCase("quit")) {
                         System.out.println("Client kill connection.");
                         break;
                     }
-
                 }
             }
-
-            System.out.println("Connection is closed.");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (Exception e)
-        {
 
+
+        // Close Chat connection
+        try {
+            closeConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
+
+
+
+
+    private void openConnection() throws IOException {
+        socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+        dataInputStream = new DataInputStream(socket.getInputStream());
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        System.out.println("Client connected with server.");
+    }
+
+
+    private void closeConnection() throws IOException {
+        dataInputStream.close();
+        dataOutputStream.close();
+        socket.close();
+        System.out.println("Connection is closed.");
+    }
+
+
+    // Internal class Thread for reading from DataInputStream
+    class ReadingClientThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (!socket.isOutputShutdown()) {
+                    outputMessage = dataInputStream.readUTF();
+                    System.out.println("Read info from server [" + outputMessage + "]");
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 
 }
--- src/Lesson6/ServerChat.java
+++ src/Lesson6/ServerChat.java
@@ -1,8 +1,6 @@
 package Lesson6;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,56 +12,115 @@ import java.net.Socket;
  */
 
 public class ServerChat {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private final int SERVER_PORT = 3345 ;
+    private DataOutputStream dataOutputStream;
+    private DataInputStream dataInputStream;
+    private BufferedReader bufferedReader;
+    private String outputMessage, inputMessage;
 
-    public static void main(String[] args)
-    {
-        try
-        {
-            System.out.println("Server is started.");
-            //Server starts on port 3345
-            ServerSocket server = new ServerSocket(3345);
 
-            // We waiting connection with client.
-            Socket client = server.accept();
-            System.out.println("Connection was successful statemented.");
 
-            // Stream uses to send message
-            DataOutputStream dataOutputStream = new DataOutputStream(client.getOutputStream());
-            // Stream uses to receive message
-            DataInputStream dataInputStream = new DataInputStream(client.getInputStream());
 
-            while (!client.isClosed())
-            {
-                dataOutputStream.writeUTF("Server is waiting info from client...");
-                dataOutputStream.flush();
-                System.out.println("Server is waiting info from client...");
+    public void start() {
 
-                String inputMessage = dataInputStream.readUTF();
-                System.out.println("Read info from client [" + inputMessage + "]");
-                dataOutputStream.writeUTF("Server accept message [" + inputMessage + "] from client.");
-                dataOutputStream.flush();
-
-                if (inputMessage.equalsIgnoreCase("quit"))
-                {
-                    System.out.println("Received [quit] message from client and session will be closed.");
-                    dataOutputStream.writeUTF("Received [quit] message from client and session will be closed.");
-                    dataOutputStream.flush();
-                    break;
-                }
-
-            }
-
-            System.out.println("Client disconnected");
-            dataInputStream.close();
-            dataOutputStream.close();
-            client.close();
-            System.out.println("Server is stopped.");
-
-        }
-        catch (IOException e)
-        {
-            System.out.println("Something is wrong!");
+        // Open Chat connection
+        try {
+            openConnection();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // Reading from DataInputStream
+        Thread readingServerTread = new ServerChat.ReadingServerThread();
+        readingServerTread.start();
+
+
+        // Writing to the DataOutputStream
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            while (!socket.isOutputShutdown()) {
+                if (bufferedReader.ready()) {
+                    inputMessage = bufferedReader.readLine();
+
+                    dataOutputStream.writeUTF(inputMessage);
+                    dataOutputStream.flush();
+                    System.out.println("Server sent message [" + inputMessage + "]");
+                    Thread.sleep(1000);
+
+                    if (inputMessage.equalsIgnoreCase("quit")) {
+                        System.out.println("Server kill connection.");
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        // Close Chat connection
+        try {
+            closeConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
+
+
+    private void openConnection() throws IOException {
+        serverSocket = new ServerSocket(SERVER_PORT);
+        System.out.println("Server is started.");
+        // We waiting connection with client.
+        Socket socket = serverSocket.accept();
+        dataInputStream = new DataInputStream(socket.getInputStream());
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        System.out.println("Connection was successful statemented.");
+    }
+
+
+    private void closeConnection() throws IOException {
+        dataInputStream.close();
+        dataOutputStream.close();
+        socket.close();
+        System.out.println("Connection is closed.");
+        System.out.println("Server is stopped.");
+
+    }
+
+
+    // Internal class Thread for reading from DataInputStream
+    class ReadingServerThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (!socket.isOutputShutdown()) {
+                    outputMessage = dataInputStream.readUTF();
+                    System.out.println("Read info from client [" + outputMessage + "]");
+                    Thread.sleep(1000);
+                    if (inputMessage.equalsIgnoreCase("quit"))
+                    {
+                        System.out.println("Received [quit] message from client and session will be closed.");
+                        dataOutputStream.writeUTF("Received [quit] message from client and session will be closed.");
+                        dataOutputStream.flush();
+                        break;
+                    }
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new ServerChat.start();
+    }
+
 }
