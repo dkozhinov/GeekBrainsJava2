42b5311769d8f41588213ac579bb714ccbf65414
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-03-07 21:39:33 +1100

    Загрузил код преподавателя Ильназа Шайхутдинова для класса ClientHandler применил интерфейс Runnable

--- src/Lesson7/ClientHandler.java
+++ src/Lesson7/ClientHandler.java
@@ -1,11 +1,97 @@
 package Lesson7;
 
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
- * Java. Level 2. Lesson X. Homework.
+ * Java. Level 2. Lesson 7. Homework.
  *
- * @author Dmitry Kozhinov d.kozhinov@mail.ru
+ * @modify by Dmitry Kozhinov d.kozhinov@mail.ru
  * Created on 07.03.2019
+ * @author ilnaz-92@yandex.ru
+ * Created on 2019-03-04
+ *
  */
 
-public class ClientHandler {
+public class ClientHandler implements Runnable {
+    private Socket clientSocket;
+    private Server server;
+    private PrintWriter outMsg;
+    private Scanner inMsg;
+    private static int clientCount = 0;
+
+
+    public ClientHandler(Socket clientSocket, Server server)
+    {
+        try
+        {
+            clientCount++;
+            this.clientSocket = clientSocket;
+            this.server = server;
+            this.outMsg = new PrintWriter(clientSocket.getOutputStream());
+            this.inMsg = new Scanner(clientSocket.getInputStream());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            server.notificationAllClientWithNewMessage("New client in our chat");
+            server.notificationAllClientWithNewMessage("In our chat = " + clientCount + "clients!");
+
+
+            while (true)
+            {
+                if (inMsg.hasNext())
+                {
+                    String clientMsg = inMsg.nextLine();
+                    if (clientMsg.equalsIgnoreCase("QUIT"))
+                    {
+                        break;
+                    }
+                    System.out.println(clientMsg);
+                    server.notificationAllClientWithNewMessage(clientMsg);
+                }
+            }
+
+            Thread.sleep(1000);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            exitFromChat();
+        }
+
+    }
+
+    private void exitFromChat()
+    {
+        clientCount--;
+        server.notificationAllClientWithNewMessage("Client exited. In out chat = " + clientCount + " clients!");
+        server.removeClient(this);
+    }
+
+    public void sendMessage(String msg)
+    {
+        try
+        {
+            outMsg.println(msg);
+            outMsg.flush();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/Lesson7/Server.java
+++ src/Lesson7/Server.java
@@ -1,11 +1,74 @@
 package Lesson7;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Java. Level 2. Lesson X. Homework.
+ * Java. Level 2. Lesson 7. Homework.
  *
- * @author Dmitry Kozhinov d.kozhinov@mail.ru
+ * @modify by Dmitry Kozhinov d.kozhinov@mail.ru
  * Created on 07.03.2019
+ * @author ilnaz-92@yandex.ru
+ * Created on 2019-03-04
+ *
  */
 
 public class Server {
+    private List<ClientHandler> clientHandlers = new ArrayList<>();
+
+    public Server()
+    {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        try
+        {
+            serverSocket = new ServerSocket(8888);
+            System.out.println("Server launched");
+
+            while (true)
+            {
+                clientSocket = serverSocket.accept();
+                ClientHandler client = new ClientHandler(clientSocket, this);
+                clientHandlers.add(client);
+                new Thread(client).start();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                serverSocket.close();
+                clientSocket.close();
+                System.out.println("Server finished");
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public void notificationAllClientWithNewMessage(String msg)
+    {
+        for (ClientHandler clientHandler : clientHandlers)
+        {
+            clientHandler.sendMessage(msg);
+        }
+
+    }
+
+    public void removeClient(ClientHandler clientHandler)
+    {
+        clientHandlers.remove(clientHandler);
+    }
+
+
 }
